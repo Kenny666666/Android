34123c337db7914df7237f7bb43c5bfcfb99543f
Kenny666666 <1525090500@qq.com>
2015-07-25 17:47:18 +0800

    各种设计模式介绍，及使用demo

--- DesignPattern/AdapterPattern/src/com/demo/MainActivity.java
+++ DesignPattern/AdapterPattern/src/com/demo/MainActivity.java
@@ -13,6 +13,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 /**
+ 
  * 适配器模式将某个类的接口转换成客户端期望的另一个接口表示，目的是消除由于接口不匹配所造成的类的兼容性问题。
  * @Description: TODO 主要分为三类：类的适配器模式、对象的适配器模式、接口的适配器模式。
  * 
